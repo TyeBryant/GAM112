d6f6b24ab961d7e6a5c4e40aaec1a3904de44f6a
Tye Bryant <Tye Bryant>
2016-06-21 17:17:17 +1000

    Added working slider for acceleration and Speedo

--- GAM112proto/Assets/Scripts/Scr_VehicleController.cs
+++ GAM112proto/Assets/Scripts/Scr_VehicleController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Scr_VehicleController : MonoBehaviour {
 
     public float accelTorque = 25f; // Effectively controls the vehicle's speed
@@ -9,7 +11,11 @@ public class Scr_VehicleController : MonoBehaviour {
 
     public WheelCollider[] Wheels; // Array to store all the wheels
 
-    private float throttlePercentage = 1.0f; // Number between 0 and 1. Controls how much of the accelTorque to use
+    public Text speedo;
+
+    private float throttlePercentage = 0.0f; // Number between 0 and 1. Controls how much of the accelTorque to use
+
+    private Rigidbody rb;
 
     // Use this for initialization
     void Start () {
@@ -18,23 +24,18 @@ public class Scr_VehicleController : MonoBehaviour {
             wheel.suspensionDistance = maxSuspensionDistance;
             wheel.radius = wheelSize;
         }
+        rb = GetComponent<Rigidbody>();
     }
 	
 	// FixedUpdate is called once per frame
 	void FixedUpdate () {
         UpdateVehicleMovement();
+        VelocityUpdater();
 	}
 
     // Called from FixedUpdate. Handles moving the vehicle.
     void UpdateVehicleMovement()
     {
-        //TEMPORARY
-        //Sets the throttle to 1 if space is down, and 0 if it's not
-        if (Input.GetKey("space"))
-            updateThrottlePercentage(1.0f);
-        else
-            updateThrottlePercentage(0.0f);
-
         float scaledTorque = throttlePercentage * accelTorque; // Scale the tourqe based on the throttle percentage
 
         foreach (WheelCollider wheel in Wheels)
@@ -44,8 +45,16 @@ public class Scr_VehicleController : MonoBehaviour {
         }
     }
 
-    void updateThrottlePercentage(float newThrottlePercentage)
+    // Accessed directly by UI slider
+    public void updateThrottlePercentage(float newThrottlePercentage)
     {
         throttlePercentage = newThrottlePercentage;
+    }
+
+    void VelocityUpdater ()
+    {
+        float currentVelocity;
+        currentVelocity = Vector3.Magnitude(rb.velocity);
+        speedo.text = (int)currentVelocity + "m/s";
     }
 }
