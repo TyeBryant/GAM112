63afcb46a4d5bd5c5e61a452cf87882421660f97
Dakota Wood <1006687@student.sae.edu.au>
2016-06-21 14:23:13 +1000

    Implemented a WheelController script, which simply repositions the wheel meshes to be aligned with the ground. Altered the global gravity to fix realism issues. May need to be tweaked still. Currently having issues making the vehicle actually move. Hopefully I can resolve these soon.

--- GAM112proto/Assets/Scripts/Scr_VehicleController.cs
+++ GAM112proto/Assets/Scripts/Scr_VehicleController.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class Scr_VehicleController : MonoBehaviour {
 
     public float accelTorque = 25f; // Effectively controls the vehicle's speed
-    public float brakeTorque = 100f; // Controls brake speed
+    public float maxSuspensionDistance = 5.0f; // How far down does the wheel's suspension reach
 
-    private float throttlePercentage = 0.0f; // Number between 0 and 1. Controls how much of the accelTorque to use
+    public WheelCollider[] Wheels; // Array to store all the wheels
+
+    private float throttlePercentage = 1.0f; // Number between 0 and 1. Controls how much of the accelTorque to use
 
     // Use this for initialization
     void Start () {
+        foreach (WheelCollider wheel in Wheels)
+        {
+            wheel.suspensionDistance = maxSuspensionDistance;
+        }
+    }
 	
-	}
-	
-	// Update is called once per frame
+	// FixedUpdate is called once per frame
 	void FixedUpdate () {
         UpdateVehicleMovement();
 	}
@@ -22,6 +27,12 @@ public class Scr_VehicleController : MonoBehaviour {
     void UpdateVehicleMovement()
     {
         float scaledTorque = throttlePercentage * accelTorque; // Scale the tourqe based on the throttle percentage
+
+        foreach(WheelCollider wheel in Wheels)
+        {
+            wheel.motorTorque = accelTorque;
+            wheel.brakeTorque = 0;
+        }
     }
 
     void updateThrottlePercentage(float newThrottlePercentage)
